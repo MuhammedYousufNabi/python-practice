d6dc98cd250e696228a2180bfe66467171ff3f8c
MuhammedYousufNabi <mdyousuf35.bd@gmail.com>
2024-12-28 17:43:03 +0600

    first commite here

---
+++ first.py
@@ -0,0 +1,30 @@
+#print("Hello world)
+
+# name = input("Whats your name: ")
+# age = input("How old are you: ")    
+# number = input("Enter a number: ")
+
+# print("Name     : " + name)
+# print("Age      : " + age)
+# print("number   : " + number)
+# my_age = "Muhammed Yousuf Nabi"
+# print(my_age)
+
+# my_age = 25
+# my_age += 4
+# print(my_age)
+
+# name = input("Enter a Your Namr: ")
+# len_name = len(name)
+# print(len_name)
+
+# print("length of the String: "+ str(len(input("Enter a String:"))))
+
+# a = input()
+# b = input()
+
+# temp = a
+# a = b
+# b = temp
+
+# print("a: " + a +"\nb: " + b)
